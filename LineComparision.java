369728548eb1a110a708aa0a6724c447d7b614b6
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-12 18:00:30 +0530

    Added: We can Compare Lines are Greater,Equal or Smaller in Length

--- LineComparision.java
+++ LineComparision.java
@@ -11,7 +11,7 @@ public class LineComparision {
 		System.out.println("Welcome to Line Comparision Program");
 		
 		
-		double line1,line2;
+		double line1,line2,difference;
 		System.out.println("Getting Coordinates of Line 1");
 		getPoints();
 		line1=calculateLength(x1, y1, x2, y2);
@@ -20,11 +20,13 @@ public class LineComparision {
 		getPoints();
 		line2=calculateLength(x1, y1, x2, y2);
 		
-		if(line1==line2)
+		difference=line1-line2;
+		if(difference==0)
 			System.out.println("Both The Lines Are EQUAL In Length!!");
+		else if(difference>0)
+			System.out.println("Line1 is Greater Than Line2 ");
 		else
-			System.out.println("Both The Lines Are oF DIFFERENT Length!!");
-		
+			System.out.println("Line1 is Smaller Than Line2 ");
 	}
 	
 	static void getPoints()
@@ -37,7 +39,7 @@ public class LineComparision {
 		x2=scan.nextInt();
 		y2=scan.nextInt();
 		
-		System.out.println("\nThese are the coordinates ("+x1+","+y1+") and ("+x2+","+y2+")\n");
+//		System.out.println("\nThese are the coordinates ("+x1+","+y1+") and ("+x2+","+y2+")\n");
 	}
 	
 	static double calculateLength(int x1,int y1,int x2,int y2)
@@ -45,6 +47,7 @@ public class LineComparision {
 		
 		double leng;
 		leng=Math.pow(Math.pow(x2-x1,2) + Math.pow(y2-y1,2),0.5);
+		System.out.println("\nThese are the coordinates ("+x1+","+y1+") and ("+x2+","+y2+") and the Length is "+leng+"\n");
 		return leng;
 	}
 
