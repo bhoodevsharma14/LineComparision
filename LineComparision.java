148dd3add66c1c8140ff210633d29fb6ab33646e
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2021-05-14 16:47:45 +0530

    Added:Refactor Code According To  OOPS Concepts Applied

--- LineComparision.java
+++ LineComparision.java
@@ -4,45 +4,36 @@ import java.util.*;
 
 public class LineComparision {
 	
-	static int x1,y1,x2,y2;
+	int x1,y1,x2,y2;	
+	double length;
 	
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Line Comparision Program");
 		
+		LineComparision line1 = new LineComparision();
+		line1.getPoints();
+		line1.length=line1.calculateLength(line1.x1, line1.y1, line1.x2, line1.y2);
 		
-		double line1,line2,difference;
-		System.out.println("Getting Coordinates of Line 1");
-		getPoints();
-		line1=calculateLength(x1, y1, x2, y2);
+		LineComparision line2 = new LineComparision();
+		line2.getPoints();
+		line2.length=line2.calculateLength(line2.x1, line2.y1, line2.x2, line2.y2);
 		
-		System.out.println("Getting Coordinates of Line 2");
-		getPoints();
-		line2=calculateLength(x1, y1, x2, y2);
-		
-		difference=line1-line2;
-		if(difference==0)
-			System.out.println("Both The Lines Are EQUAL In Length!!");
-		else if(difference>0)
-			System.out.println("Line1 is Greater Than Line2 ");
-		else
-			System.out.println("Line1 is Smaller Than Line2 ");
+		compareTwoLines(line1.length, line2.length);
 	}
 	
-	static void getPoints()
+	public void getPoints()
 	{
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please Enter coordinate of point 1: x1 and y1");
-		x1=scan.nextInt();
-		y1=scan.nextInt();
+		this.x1=scan.nextInt();
+		this.y1=scan.nextInt();
 		System.out.println("Please Enter coordinate of point 2: x2 and y2");
-		x2=scan.nextInt();
-		y2=scan.nextInt();
-		
-//		System.out.println("\nThese are the coordinates ("+x1+","+y1+") and ("+x2+","+y2+")\n");
+		this.x2=scan.nextInt();
+		this.y2=scan.nextInt();		
 	}
 	
-	static double calculateLength(int x1,int y1,int x2,int y2)
+	public double calculateLength(int x1,int y1,int x2,int y2)
 	{
 		
 		double leng;
@@ -51,4 +42,14 @@ public class LineComparision {
 		return leng;
 	}
 
+	public static void compareTwoLines(double l1,double l2)
+	{
+		double difference=l1-l2;
+		if(difference==0)
+			System.out.println("Both The Lines Are EQUAL In Length!!");
+		else if(difference>0)
+			System.out.println("Line1 is Greater Than Line2 ");
+		else
+			System.out.println("Line1 is Smaller Than Line2 ");
+	}
 }
